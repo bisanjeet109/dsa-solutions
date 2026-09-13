337c5c9a043506b86a0488dc435e21bef2574569
Bisanjeet Mohapatra <bisanjeet109@gmail.com>
2026-09-13 12:09:01 +0530

    Time: 2 ms (81.45%), Space: 12.4 MB (72.88%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+
+        for i in s:
+            if i == "(":
+                stack.append(")")
+            elif i == "[":
+                stack.append("]")
+            elif i == "{":
+                stack.append("}")
+            else:
+                if not stack or stack.pop() != i:
+                    return False
+
+        return len(stack) == 0
